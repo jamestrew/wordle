e81ce7e4178adfc905d9b8130647cb1d32ddc5ba
James Trew <j.trew10@gmail.com>
2022-05-03 20:18:08 -0400

    start some shit

--- src/wordle.c
+++ src/wordle.c
@@ -46,18 +46,13 @@
     └───────────────────────────────────────┘
 */
 
-void getPlayWord(char *testWord);
-
-WINDOW *create_newwin(int height, int width, int starty, int startx,
-                      char text) {
-  // allocated window in mem
-  WINDOW *local_win = newwin(height, width, starty, startx);
-
-  box(local_win, 0, 0); // draws box
-  mvwprintw(local_win, height / 2, width / 2, "%c", text);
-  wrefresh(local_win);
-  return local_win;
-}
+void getPlayWord(char *playWord);
+WINDOW *initBoard();
+void handleArrows(WINDOW *game_win, int direction);
+void handleEnter(WINDOW *game_win);
+void handleBackspace(WINDOW *game_win);
+void handleLetters(WINDOW *game_win, int ch);
+int playRound(int roundNumber, const char *const playWord);
 
 int main() {
   char playWord[WORD_LENGTH + 2]; // newline and null
@@ -65,36 +60,29 @@ int main() {
   getPlayWord(playWord);
 
   initscr();
-  raw();
-  noecho();
-  keypad(stdscr, TRUE);
-  start_color();
-  cbreak();
-  curs_set(0);
-  init_pair(1, COLOR_CYAN, COLOR_BLACK);
-  init_pair(2, COLOR_RED, COLOR_BLACK);
-  refresh();
-
-  WINDOW *game_win = newwin(BOARD_HEIGHT, BOARD_WIDTH, 0, 0);
-  keypad(game_win, TRUE);
-  box(game_win, 0, 0);
-  wattron(game_win, COLOR_PAIR(1));
-  mvwprintw(game_win, 1, 1, "ENTER GUESS:");
-  wattroff(game_win, COLOR_PAIR(1));
-
-  mvwprintw(game_win, 2, 1, "> %c %c %c %c %c", playWord[0], playWord[1],
-            playWord[2], playWord[3], playWord[4]);
-
-  wattron(game_win, COLOR_PAIR(2));
-  mvwprintw(game_win, BOARD_HEIGHT - 2, 1, "Press F1 to quit");
-  wattroff(game_win, COLOR_PAIR(2));
-  wrefresh(game_win);
-
+  WINDOW *game_win = initBoard();
 
   while ((ch = getch()) != KEY_F(1)) {
-    printw("%c", ch);
+
+    switch (ch) {
+    case KEY_LEFT:
+      handleArrows(game_win, KEY_LEFT);
+      break;
+    case KEY_RIGHT:
+      handleArrows(game_win, KEY_RIGHT);
+      break;
+    case KEY_BACKSPACE:
+      handleBackspace(game_win);
+      break;
+    case KEY_ENTER:
+      handleEnter(game_win);
+      break;
+    default:
+      handleLetters(game_win, ch);
+    }
   }
 
+  delwin(game_win);
   endwin();
 }
 
@@ -117,4 +105,30 @@ void getPlayWord(char *playWord) {
   }
   playWord[WORD_LENGTH] = '\0';
   fclose(fp);
+}
+
+WINDOW *initBoard() {
+  noecho();
+  keypad(stdscr, TRUE);
+  start_color();
+  cbreak();
+  // curs_set(0);
+  init_pair(1, COLOR_CYAN, COLOR_BLACK);
+  init_pair(2, COLOR_RED, COLOR_BLACK);
+  refresh();
+
+  WINDOW *local_win = newwin(BOARD_HEIGHT, BOARD_WIDTH, 0, 0);
+  box(local_win, 0, 0);
+
+  wattron(local_win, COLOR_PAIR(1));
+  mvwprintw(local_win, 1, 1, "ENTER GUESS:");
+  wattroff(local_win, COLOR_PAIR(1));
+
+  wattron(local_win, COLOR_PAIR(2));
+  mvwprintw(local_win, BOARD_HEIGHT - 2, 1, "Press F1 to quit");
+  wattroff(local_win, COLOR_PAIR(2));
+
+  mvwprintw(local_win, START_ROW, START_COL, "> ");
+  wrefresh(local_win);
+  return local_win;
 }
