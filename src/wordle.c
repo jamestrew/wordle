807f9c3fb6c923f8e893376d107ef2848991aa2c
James Trew <j.trew10@gmail.com>
2022-05-05 22:15:04 -0400

    split up header

--- src/wordle.c
+++ src/wordle.c
@@ -4,64 +4,7 @@
 #include <string.h>
 #include <time.h>
 
-#define WORD_COUNT 659
-#define WORD_LENGTH 5
-
-#define GUESS_COUNT 6
-#define X_SPACING 2
-#define Y_SPACING 2
-
-#define BOARD_HEIGHT 25
-#define BOARD_WIDTH 42
-
-#define START_COL 3
-#define START_ROW 3
-#define END_COL START_COL + (WORD_LENGTH - 1) * X_SPACING
-#define END_ROW START_ROW + (GUESS_COUNT - 1) * Y_SPACING
-
-/* TODO:
-   [x] get random word
-   [ ] create basic play board
-   [ ] create input handling
-   [ ] do the letter coloring
-   [ ] do the wordle colorful thing
-
-   - 6 letters
-   - green for correct guesses
-   - yellow for correct but wrong positions
-   - repeats aren't additinally yellow
-
-   - black bg
-    ┌───────────────────────────────────────┐
-    │                                       │
-    │ENTER GUESS:                           │
-    │                                       │
-    │> A A A A A   press enter to confirm   │
-    │                                       │
-    │                                       │
-    │                                       │
-    │                                       │
-    │                                       │
-    │                                       │
-    │                                       │
-    │                                       │
-    │                                       │
-    │                                       │
-    │                                       │
-    │                                       │
-    │                                       │
-    │                                       │
-    │                                       │
-    └───────────────────────────────────────┘
-*/
-
-void getPlayWord(char *playWord);
-WINDOW *initBoard();
-void handleArrows(WINDOW *game_win, int direction);
-void handleEnter(WINDOW *game_win);
-void handleBackspace(WINDOW *game_win);
-void handleLetters(WINDOW *game_win, int ch);
-void debugCursoryx(WINDOW *game_win);
+#include "wordle.h"
 
 int main() {
   char playWord[WORD_LENGTH + 2]; // newline and null
